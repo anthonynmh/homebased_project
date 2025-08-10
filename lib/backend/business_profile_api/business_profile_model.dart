21674b02bea5813943cc81ef5066ba676436d27b
anthonynmh <anthonyneomh@gmail.com>
2025-08-11 04:01:24 +0800

    develop business profile api

--- lib/backend/business_profile_api/business_profile_model.dart
+++ lib/backend/business_profile_api/business_profile_model.dart
@@ -1,19 +1,72 @@
 class BusinessProfile {
-  final String id;
-  final String businessName;
-  final String? logoUrl;
+  final int id;
+  final DateTime? createdAt;
+  final String name;
+  final String? description;
+  final String? sector;
+  final String? photosUrl;
+  final String auth0Sub;
+  final String? location;
 
-  BusinessProfile({required this.id, required this.businessName, this.logoUrl});
+  BusinessProfile({
+    required this.id,
+    this.createdAt,
+    required this.name,
+    this.description,
+    this.sector,
+    this.photosUrl,
+    required this.auth0Sub,
+    this.location,
+  });
 
   factory BusinessProfile.fromMap(Map<String, dynamic> map) {
     return BusinessProfile(
-      id: map['id'] as String,
-      businessName: map['business_name'] as String,
-      logoUrl: map['logo_url'] as String?,
+      id: map['id'] as int,
+      createdAt: map['created_at'] != null
+          ? DateTime.parse(map['created_at'] as String)
+          : null,
+      name: (map['name'] ?? '') as String,
+      description: map['description'] as String?,
+      sector: map['sector'] as String?,
+      photosUrl: map['photos_url'] as String?,
+      auth0Sub: map['auth0_sub'] as String,
+      location: map['location'] as String?,
     );
   }
 
   Map<String, dynamic> toMap() {
-    return {'id': id, 'business_name': businessName, 'logo_url': logoUrl};
+    return {
+      'id': id,
+      if (createdAt != null) 'created_at': createdAt!.toIso8601String(),
+      'name': name,
+      'description': description,
+      'sector': sector,
+      'photos_url': photosUrl,
+      'auth0_sub': auth0Sub,
+      'location': location,
+    };
+  }
+
+  // Create a copyWith method to easily update auth0Sub
+  BusinessProfile copyWith({
+    int? id,
+    DateTime? createdAt,
+    String? name,
+    String? description,
+    String? sector,
+    String? photosUrl,
+    String? auth0Sub,
+    String? location,
+  }) {
+    return BusinessProfile(
+      id: id ?? this.id,
+      createdAt: createdAt ?? this.createdAt,
+      name: name ?? this.name,
+      description: description ?? this.description,
+      sector: sector ?? this.sector,
+      photosUrl: photosUrl ?? this.photosUrl,
+      auth0Sub: auth0Sub ?? this.auth0Sub,
+      location: location ?? this.location,
+    );
   }
 }
