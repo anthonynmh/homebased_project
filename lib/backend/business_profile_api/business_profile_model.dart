cafb0680497689644330f56d2c5e1b56425e9c2d
anthonynmh <anthonyneomh@gmail.com>
2025-09-07 22:24:26 +0800

    develop business profile api

--- lib/backend/business_profile_api/business_profile_model.dart
+++ lib/backend/business_profile_api/business_profile_model.dart
@@ -1,22 +1,33 @@
+import 'dart:convert';
+
 class BusinessProfile {
-  final String id;
-  final String businessName;
+  final String? id;
+  final String? updatedAt;
+  final String? businessName;
+  final String? description;
+  final String? sector;
   final double? latitude;
   final double? longitude;
   final String? logoUrl;
 
   BusinessProfile({
-    required this.id, 
-    required this.businessName, 
-    this.latitude, 
-    this.longitude, 
+    this.id,
+    this.updatedAt,
+    this.businessName,
+    this.description,
+    this.sector,
+    this.latitude,
+    this.longitude,
     this.logoUrl,
   });
 
   factory BusinessProfile.fromMap(Map<String, dynamic> map) {
     return BusinessProfile(
       id: map['id'] as String,
+      updatedAt: map['updated_at'] as String?,
       businessName: map['business_name'] as String,
+      description: map['description'] as String,
+      sector: map['sector'] as String,
       latitude: map['latitude'] as double?,
       longitude: map['longitude'] as double?,
       logoUrl: map['logo_url'] as String?,
@@ -24,6 +35,20 @@ class BusinessProfile {
   }
 
   Map<String, dynamic> toMap() {
-    return {'id': id, 'business_name': businessName, 'logo_url': logoUrl};
+    return {
+      'id': id,
+      'updated_at': updatedAt,
+      'business_name': businessName,
+      'description': description,
+      'sector': sector,
+      'longitude': longitude,
+      'latitude': latitude,
+      'logo_url': logoUrl,
+    };
+  }
+
+  @override
+  String toString() {
+    return const JsonEncoder.withIndent('  ').convert(toMap());
   }
 }
