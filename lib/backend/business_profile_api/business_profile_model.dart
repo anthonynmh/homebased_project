0a3a33808317526b2bd483e01a9c6bd2ea0a8927
anthonynmh <anthonyneomh@gmail.com>
2025-11-10 18:17:05 +0800

    update business schema

--- lib/backend/business_profile_api/business_profile_model.dart
+++ lib/backend/business_profile_api/business_profile_model.dart
@@ -1,39 +1,27 @@
 import 'dart:convert';
 
 class BusinessProfile {
-  final String? id;
-  final String? updatedAt;
-  final String? businessName;
-  final String? description;
-  final String? sector;
-  final double? latitude;
-  final double? longitude;
-  final String? logoUrl;
-  final List<dynamic>? photoUrls;
+  String id;
+  String updatedAt;
+  String businessName;
+  String? description;
+  List<String>? photoUrls;
 
   BusinessProfile({
-    this.id,
-    this.updatedAt,
-    this.businessName,
+    required this.id,
+    required this.updatedAt,
+    required this.businessName,
     this.description,
-    this.sector,
-    this.latitude,
-    this.longitude,
-    this.logoUrl,
     this.photoUrls,
   });
 
   factory BusinessProfile.fromMap(Map<String, dynamic> map) {
     return BusinessProfile(
       id: map['id'] as String,
-      updatedAt: map['updated_at'] as String?,
-      businessName: map['business_name'] as String?,
+      updatedAt: map['updated_at'] as String,
+      businessName: map['business_name'] as String,
       description: map['description'] as String?,
-      sector: map['sector'] as String?,
-      latitude: map['latitude'] as double?,
-      longitude: map['longitude'] as double?,
-      logoUrl: map['logo_url'] as String?,
-      photoUrls: map['photo_urls'] as List<dynamic>?,
+      photoUrls: map['photo_urls'] as List<String>?,
     );
   }
 
@@ -43,10 +31,6 @@ class BusinessProfile {
       'updated_at': updatedAt,
       'business_name': businessName,
       'description': description,
-      'sector': sector,
-      'longitude': longitude,
-      'latitude': latitude,
-      'logo_url': logoUrl,
       'photo_urls': photoUrls,
     };
   }
