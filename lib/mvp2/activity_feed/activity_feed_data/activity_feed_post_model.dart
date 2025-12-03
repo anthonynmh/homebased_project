1c6f17fbdf0d5a78d618cf2d6ad549fbb416c2cf
Zeli0 <elijahku716@gmail.com>
2025-12-03 15:33:19 +0800

    Add post model

---
+++ lib/mvp2/activity_feed/activity_feed_data/activity_feed_post_model.dart
@@ -0,0 +1,85 @@
+import 'dart:convert';
+
+class Author {
+  final String name;
+  final String username;
+  final String avatar;
+  final String businessName;
+
+  Author({
+    required this.name,
+    required this.username,
+    required this.avatar,
+    required this.businessName,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'username': username,
+      'avatar': avatar,
+      'businessName': businessName,
+    };
+  }
+
+  factory Author.fromMap(Map<String, dynamic> map) {
+    return Author(
+      name: map['name'] ?? '',
+      username: map['username'] ?? '',
+      avatar: map['avatar'] ?? '',
+      businessName: map['businessName'] ?? '',
+    );
+  }
+}
+
+class Post {
+  final String id;
+  final Author author;
+  final String content;
+  final String? image;
+  final String timestamp;
+  final int initialLikes;
+  final int initialReplies;
+  final bool isFollowing;
+
+  Post({
+    required this.id,
+    required this.author,
+    required this.content,
+    this.image,
+    required this.timestamp,
+    required this.initialLikes,
+    required this.initialReplies,
+    required this.isFollowing,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'author': author.toMap(),
+      'content': content,
+      'image': image,
+      'timestamp': timestamp,
+      'initialLikes': initialLikes,
+      'initialReplies': initialReplies,
+      'isFollowing': isFollowing,
+    };
+  }
+
+  factory Post.fromMap(Map<String, dynamic> map) {
+    return Post(
+      id: map['id'] ?? '',
+      author: Author.fromMap(map['author']),
+      content: map['content'] ?? '',
+      image: map['image'],
+      timestamp: map['timestamp'] ?? '',
+      initialLikes: map['initialLikes']?.toInt() ?? 0,
+      initialReplies: map['initialReplies']?.toInt() ?? 0,
+      isFollowing: map['isFollowing'] ?? false,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+  
+  factory Post.fromJson(String source) => Post.fromMap(json.decode(source));
+}
